d651077ee2d9be6fc2ea45c9d21a578f37b81409
xxaxxelxx <x@axxel.net>
2015-11-26 12:24:15 +0100

    updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,14 @@ PASSWORDFILE=$CFGDIR/lighttpd-plain.user
 CONFIGFILE=$CFGDIR/lighttpd.conf
 
 # password section
-echo "admin:$CUSTOMERPASSWORD_ADMIN" > $PASSWORDFILE
+echo "admin:$CUSTOMERPASSWORD_admin" > $PASSWORDFILE
 CONFIGSTRING=""
 for CUSTOMER in "$@"; do
     echo $CUSTOMER:$(eval echo \$CUSTOMERPASSWORD_$CUSTOMER) >> $PASSWORDFILE
     if [ "x$CONFIGSTRING" != "x" ]; then
-	CONFIGSTRING="\"/customer/$CUSTOMER\" => (\"method\"  => \"digest\",\"realm\"   => \"You are entering the $CUSTOMER sector!\",\"require\" => \"user=$CUSTOMER\" ),$CONFIGSTRING"
+	CONFIGSTRING="\"/$CUSTOMER\" => (\"method\"  => \"digest\",\"realm\"   => \"You are entering the $CUSTOMER sector.\",\"require\" => \"user=admin|user=$CUSTOMER\" ),$CONFIGSTRING"
     else
-	CONFIGSTRING="\"/customer/$CUSTOMER\" => (\"method\"  => \"digest\",\"realm\"   => \"You are entering the $CUSTOMER sector!\",\"require\" => \"user=$CUSTOMER\" )"
+	CONFIGSTRING="\"/$CUSTOMER\" => (\"method\"  => \"digest\",\"realm\"   => \"You are entering the $CUSTOMER sector. Authenticate yourself to the skynet.\",\"require\" => \"user=admin|user=$CUSTOMER\" )"
     fi
     cp index.html /customer/$CUSTOMER/
 done
