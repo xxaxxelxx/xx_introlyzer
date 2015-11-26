2bfeaa648e24ae7d27240a7bac4bfbf9347ad3c1
xxaxxelxx <x@axxel.net>
2015-11-26 13:53:11 +0100

    updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,13 @@ CONFIGFILE=$CFGDIR/lighttpd.conf
 echo "admin:$CUSTOMERPASSWORD_admin" > $PASSWORDFILE
 CONFIGSTRING="\"/admin\" => (\"method\"  => \"digest\",\"realm\"   => \"You are entering the admin sector. Authenticate yourself to the skynet.\",\"require\" => \"user=admin\" )"
 USERSTRING="user=admin"
+test -d /customer/admin/ || mkdir -p /customer/admin/
+cp index.admin.html /customer/admin/
 for CUSTOMER in "$@"; do
     echo $CUSTOMER:$(eval echo \$CUSTOMERPASSWORD_$CUSTOMER) >> $PASSWORDFILE
     CONFIGSTRING="\"/$CUSTOMER\" => (\"method\"  => \"digest\",\"realm\"   => \"You are entering the $CUSTOMER sector. Authenticate yourself to the skynet.\",\"require\" => \"user=admin|user=$CUSTOMER\" ),$CONFIGSTRING"
-    cp index.html /customer/$CUSTOMER/
+    cp index.customer.html /customer/$CUSTOMER/index.html
+    sed -i "s/CUSTOMER/$CUSTOMER/g" /customer/$CUSTOMER/index.html
     USERSTRING="$USERSTRING|user=$CUSTOMER"
 done
 CONFIGSTRING="\"/\" => (\"method\"  => \"digest\",\"realm\"   => \"Authenticate yourself to the skynet.\",\"require\" => \"$USERSTRING\" ),$CONFIGSTRING"
