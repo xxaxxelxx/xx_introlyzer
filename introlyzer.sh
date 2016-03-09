e0e7ce69c7e6610bbdd66bec8a488eeda08232a7
xxaxxelxx <x@axxel.net>
2016-03-09 16:04:17 +0100

    updated

--- introlyzer.sh
+++ introlyzer.sh
@@ -63,107 +63,90 @@ while true; do
     for RAWINTROLOG in $DEPOTDIR/introlog.${CUSTOMER}.*; do
 	test -r "$RAWINTROLOG" || continue
 cp -f "$RAWINTROLOG" $DEPOTDIR/introtest/
-#echo "$RAWINTROLOG"
-	TOFFSET=0
-	while [ $TOFFSET -lt $((86400 * 10)) ]; do
-	    NOW=$(date +%s)
-	    TSTAMP_YMD=$(date -d @$(($NOW - $TOFFSET)) +%Y-%m-%d)
-	    TSTAMP_YM=$(date -d @$(($NOW - $TOFFSET)) +%Y-%m)
-	    TSTAMPLOG=$(date -d @$(($NOW - $TOFFSET)) +%d/.../%Y)
-	    FILE_SQLCMD=intro.$CUSTOMER.$TSTAMP_YMD.sql
-	    FILE_SQLITE=intro.$CUSTOMER.$TSTAMP_YM.sqlite
-	    FILE_CSV=intro.$CUSTOMER.$TSTAMP_YMD.csv.txt
 
-	    find $INTRODIR -type f -mtime +${MAXAGE} -exec rm {} \;
+	FILE_BASE="intro.$CUSTOMER"
+	FILE_SQLCMD_SUFF="sql"
+	FILE_SQLITE_SUFF="ite"
+	FILE_CSV_SUFF="csv.txt"
 
-	    test -r $INTRODIR/$FILE_SQLITE || create_database "$INTRODIR/$FILE_SQLITE"
+	find $INTRODIR -type f -mtime +${MAXAGE} -iname "introlog*" -exec rm {} \;
 
-	    cat "$RAWINTROLOG" | ( \
-		C_IP=""; C_DUR=""; C_MOUNT=""
-		while read LINE; do
-			echo "$LINE" | grep "$TSTAMPLOG" > /dev/null || continue
-	    		C_IP=$(echo "$LINE" | awk '{print $1}')
-	    		C_DUR=$(echo "${LINE##*\ }")
-	    		C_MOUNT=$(echo "$LINE" | awk '{print $7}' | sed 's|^/||')
-	    		C_CHANNEL=$(echo "$C_MOUNT" | sed 's|\..*||')
-	    		C_TIME=$(echo "$LINE" | awk '{print $4}' | sed 's|\[||')
-	    		if [ "x$C_IP" == "x" ]; then continue; fi
-	    		if [ "x$C_DUR" == "x" ]; then continue; fi
-	    		[[ $C_DUR =~ ^[-+]?[0-9]+$ ]]; if [ $? -ne 0 ]; then continue; fi
-	    		if [ "x$C_MOUNT" == "x" ]; then continue; fi
-	    		if [ "x$C_TIME" == "x" ]; then continue; fi
-	    		A_TIME=($(echo "$C_TIME" | sed 's|\/|\ |g' | sed 's|\:|\ |g'))
-	    		C_T_YEAR=${A_TIME[2]}
-	    		C_T_MONTH=${A_TIME[1]}
-	    		C_T_DAY=${A_TIME[0]}
-#	    		C_T_DAY_INT=${A_TIME[0]#0*}
-	    		C_T_HOUR=${A_TIME[3]}
-#	    		C_T_HOUR_INT=${A_TIME[3]#0*}
-	    		C_T_MIN=${A_TIME[4]}
-	    		C_T_SEC=${A_TIME[5]}
-	    		C_T_EPOCH=$(date -d "${C_T_MONTH} ${C_T_DAY} ${C_T_HOUR}:${C_T_MIN}:${C_T_SEC} ${C_T_YEAR} UTC" +%s)
-	    		C_T_E_STOP="$C_T_EPOCH"
-	    		C_T_E_START=$(($C_T_EPOCH - $C_DUR))
-	    		C_T_STOP="$(date -d @${C_T_E_STOP} '+%Y-%m-%d %H:%M:%S')"
-	    		C_T_START="$(date -d @${C_T_E_START} '+%Y-%m-%d %H:%M:%S')"
-	    		C_T_START_DATE="$(date -d @${C_T_E_START} '+%Y-%m-%d')"
-	    		C_T_START_TIME="$(date -d @${C_T_E_START} '+%H:%M:%S')"
-			C_T_START_HOUR="$(date -d @${C_T_E_START} +%H)"
-			C_T_START_HOUR_INT="${C_T_START_HOUR#0*}"
-			C_T_START_DAY="$(date -d @${C_T_E_START} +%d)"
-			C_T_START_DAY_INT="${C_T_START_DAY#0*}"
-#geoiplookup $C_IP | grep 'City' | sed 's|^.*\:||' | grep -i 'sevastop' || continue
-#	    		OIFS=$IFS;IFS=$'\,';A_GEO=($(geoiplookup $C_IP | grep 'City' | sed 's|^.*\:||' 2>/dev/null));IFS=$OIFS
-			QUOTE="\'"
-	    		OIFS=$IFS;IFS=$'\,';A_GEO=($(geoiplookup $C_IP | grep 'City' | sed 's|^.*\:||' | sed 's|\x27||g' 2>/dev/null));IFS=$OIFS
-			C_GEO_COUNTRY="$(echo "${A_GEO[0]}" | sed 's|^\ ||')"
-			C_GEO_STATE="$(echo "${A_GEO[2]}" | sed 's|^\ ||')"
-			C_GEO_CITY="$(echo "${A_GEO[3]}" | sed 's|^\ ||')"
-#echo "$C_GEO_STATE"
-			C_GEO_ZIP="$(echo "${A_GEO[4]}" | sed 's|^\ ||')"
-			C_GEO_LAT="$(echo "${A_GEO[5]}" | sed 's|^\ ||')"
-			C_GEO_LON="$(echo "${A_GEO[6]}" | sed 's|^\ ||')"
 
-			CSVSTRING="${C_MOUNT},${C_CHANNEL},${C_IP},${C_T_START},${C_T_START_DATE},${C_T_START_TIME},${C_T_START_HOUR},${C_DUR},${C_GEO_COUNTRY},${C_GEO_STATE},${C_GEO_CITY},${C_GEO_ZIP},${C_GEO_LAT},${C_GEO_LON}\r"
-			SQLSTRING="INSERT INTO introstats ( mountpoint,channel,ipaddress,start_datetime,start_date,start_day_txt,start_day_int,start_time,start_hour_txt,start_hour_int,played_sec,country,state,city,zip,latitude,longitude ) VALUES ('${C_MOUNT}','${C_CHANNEL}','${C_IP}','${C_T_START}','${C_T_START_DATE}','${C_T_START_DAY}','${C_T_START_DAY_INT}','${C_T_START_TIME}','${C_T_START_HOUR}','${C_T_START_HOUR_INT}',${C_DUR},'${C_GEO_COUNTRY}','${C_GEO_STATE}','${C_GEO_CITY}','${C_GEO_ZIP}','${C_GEO_LAT}','${C_GEO_LON}');"
+	cat "$RAWINTROLOG" | ( \
+	    C_IP=""; C_DUR=""; C_MOUNT=""
+	    while read LINE; do
+		echo "$LINE" | grep "$TSTAMPLOG" > /dev/null || continue
+	    	C_IP=$(echo "$LINE" | awk '{print $1}')
+	    	C_DUR=$(echo "${LINE##*\ }")
+	    	C_MOUNT=$(echo "$LINE" | awk '{print $7}' | sed 's|^/||')
+	    	C_CHANNEL=$(echo "$C_MOUNT" | sed 's|\..*||')
+	    	C_TIME=$(echo "$LINE" | awk '{print $4}' | sed 's|\[||')
+	    	if [ "x$C_IP" == "x" ]; then continue; fi
+	    	if [ "x$C_DUR" == "x" ]; then continue; fi
+	    	[[ $C_DUR =~ ^[-+]?[0-9]+$ ]]; if [ $? -ne 0 ]; then continue; fi
+	    	if [ "x$C_MOUNT" == "x" ]; then continue; fi
+	    	if [ "x$C_TIME" == "x" ]; then continue; fi
+	    	A_TIME=($(echo "$C_TIME" | sed 's|\/|\ |g' | sed 's|\:|\ |g'))
+	    	C_T_YEAR=${A_TIME[2]}
+	    	C_T_MONTH=${A_TIME[1]}
+	    	C_T_DAY=${A_TIME[0]}
+#	    	C_T_DAY_INT=${A_TIME[0]#0*}
+	    	C_T_HOUR=${A_TIME[3]}
+#	    	C_T_HOUR_INT=${A_TIME[3]#0*}
+	    	C_T_MIN=${A_TIME[4]}
+	    	C_T_SEC=${A_TIME[5]}
+	    	C_T_EPOCH=$(date -d "${C_T_MONTH} ${C_T_DAY} ${C_T_HOUR}:${C_T_MIN}:${C_T_SEC} ${C_T_YEAR} UTC" +%s)
+	    	C_T_E_STOP="$C_T_EPOCH"
+	    	C_T_E_START=$(($C_T_EPOCH - $C_DUR))
+	    	C_T_STOP="$(date -d @${C_T_E_STOP} '+%Y-%m-%d %H:%M:%S')"
+	    	C_T_START="$(date -d @${C_T_E_START} '+%Y-%m-%d %H:%M:%S')"
+	    	C_T_START_DATE="$(date -d @${C_T_E_START} '+%Y-%m-%d')"
+	    	C_T_START_TIME="$(date -d @${C_T_E_START} '+%H:%M:%S')"
+		C_T_START_HOUR="$(date -d @${C_T_E_START} +%H)"
+		C_T_START_HOUR_INT="${C_T_START_HOUR#0*}"
+		C_T_START_DAY="$(date -d @${C_T_E_START} +%d)"
+		C_T_START_DAY_INT="${C_T_START_DAY#0*}"
+		C_T_START_YEAR="$(date -d @${C_T_E_START} +%Y)"
+		C_T_START_MONTH="$(date -d @${C_T_E_START} +%m)"
+		QUOTE="\'"
+	    	OIFS=$IFS;IFS=$'\,';A_GEO=($(geoiplookup $C_IP | grep 'City' | sed 's|^.*\:||' | sed 's|\x27||g' 2>/dev/null));IFS=$OIFS
+		C_GEO_COUNTRY="$(echo "${A_GEO[0]}" | sed 's|^\ ||')"
+		C_GEO_STATE="$(echo "${A_GEO[2]}" | sed 's|^\ ||')"
+		C_GEO_CITY="$(echo "${A_GEO[3]}" | sed 's|^\ ||')"
+		C_GEO_ZIP="$(echo "${A_GEO[4]}" | sed 's|^\ ||')"
+		C_GEO_LAT="$(echo "${A_GEO[5]}" | sed 's|^\ ||')"
+		C_GEO_LON="$(echo "${A_GEO[6]}" | sed 's|^\ ||')"
 
-#			# UTF-8 OUT ####################################################################################################
-			echo -e "$CSVSTRING" | iconv -f ascii -t utf-8 -c >> $DEPOTDIR/$FILE_CSV
-			echo -e "$SQLSTRING" | iconv -f ascii -t utf-8 -c >> $DEPOTDIR/$FILE_SQLCMD
+		CSVSTRING="${C_MOUNT},${C_CHANNEL},${C_IP},${C_T_START},${C_T_START_DATE},${C_T_START_TIME},${C_T_START_HOUR},${C_DUR},${C_GEO_COUNTRY},${C_GEO_STATE},${C_GEO_CITY},${C_GEO_ZIP},${C_GEO_LAT},${C_GEO_LON}\r"
+		SQLSTRING="INSERT INTO introstats ( mountpoint,channel,ipaddress,start_datetime,start_date,start_day_txt,start_day_int,start_time,start_hour_txt,start_hour_int,played_sec,country,state,city,zip,latitude,longitude ) VALUES ('${C_MOUNT}','${C_CHANNEL}','${C_IP}','${C_T_START}','${C_T_START_DATE}','${C_T_START_DAY}','${C_T_START_DAY_INT}','${C_T_START_TIME}','${C_T_START_HOUR}','${C_T_START_HOUR_INT}',${C_DUR},'${C_GEO_COUNTRY}','${C_GEO_STATE}','${C_GEO_CITY}','${C_GEO_ZIP}','${C_GEO_LAT}','${C_GEO_LON}');"
 
-#			# ASCII OUT ####################################################################################################
-#			echo "$CSVSTRING" | iconv -f utf-8 -t ascii -c >> $INTRODIR/$FILE_CSV
-#			SQLLOOP=0
-#			while [ $SQLLOOP -lt 10 ]; do
-#			    echo "$SQLSTRING" | iconv  -f utf-8 -t ascii -c | $SQLITE "$INTRODIR/$FILE_SQLITE" 2>/dev/null && break
-#			    SQLLOOP=$(( $SQLLOOP + 1 ))
-#			    sleep 1
-#			done
-		done
-	    )
-	    TOFFSET=$(( $TOFFSET + 86400 ))
-
-	    SQLLOOP=0
-	    if [ -r "$DEPOTDIR/$FILE_CSV" ]; then
-		cat "$DEPOTDIR/$FILE_CSV" >> "$INTRODIR/$FILE_CSV"
-		if [ $? -eq 0 ]; then
-		    rm -f "$DEPOTDIR/$FILE_CSV"
-		fi
-	    fi
-	    if [ -r "$DEPOTDIR/$FILE_SQLCMD" ]; then
-		while [ $SQLLOOP -lt 10 ]; do
-		    cat "$DEPOTDIR/$FILE_SQLCMD" | iconv -f ascii -t utf-8 -c | $SQLITE "$INTRODIR/$FILE_SQLITE" 2>/dev/null 
-		    if [ $? -eq 0 ]; then
-			rm -f "$DEPOTDIR/$FILE_SQLCMD"
-			break
-		    fi
-		    SQLLOOP=$(( $SQLLOOP + 1 ))
-		    sleep 1
-		done
-	    fi
-	    test -r $INTRODIR/$FILE_SQLITE && remove_empty_database "$INTRODIR/$FILE_SQLITE"
-	done
+#		# UTF-8 OUT ####################################################################################################
+		echo -e "$CSVSTRING" | iconv -f ascii -t utf-8 -c >> $DEPOTDIR/${FILE_BASE}.${C_T_START_YEAR}-${C_T_START_MONTH}-${C_T_START_DAY}.${FILE_CSV_SUFF}
+		echo -e "$SQLSTRING" | iconv -f ascii -t utf-8 -c >> $DEPOTDIR/${FILE_BASE}.${C_T_START_YEAR}-${C_T_START_MONTH}.${FILE_SQLCMD_SUFF}
+	    done
+	)
 	rm -f "$RAWINTROLOG"
+    done
+    for PREINTROLOG in $DEPOTDIR/${FILE_BASE}.*.${FILE_CSV_SUFF}; do
+	test -r "$PREINTROLOG" || continue
+	cat "$PREINTROLOG" >> "$INTRODIR/$(basename $PREINTROLOG)"
+	if [ $? -eq 0 ]; then
+	    rm -f "$PREINTROLOG"
+	fi
+    done
+    for PREINTROLOG in $DEPOTDIR/${FILE_BASE}.*.${FILE_SQLCMD_SUFF}; do
+	test -r "$PREINTROLOG" || continue
+	test -r "$INTRODIR/$(basename $PREINTROLOG)${FILE_SQLITE_SUFF}" || create_database "$INTRODIR/$(basename $PREINTROLOG)${FILE_SQLITE_SUFF}"
+	SQLLOOP=0
+	while [ $SQLLOOP -lt 10 ]; do
+	    cat "$PREINTROLOG" | $SQLITE "$INTRODIR/$(basename $PREINTROLOG)${FILE_SQLITE_SUFF}" 2>/dev/null 
+	    if [ $? -eq 0 ]; then
+		rm -f "$PREINTROLOG"
+		break
+	    fi
+	    SQLLOOP=$(( $SQLLOOP + 1 ))
+	    sleep 1
+	done
     done
     sleep $SLEEP
 done
